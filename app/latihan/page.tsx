2580ff37f25ab27c161e343804e4e77fd4f77c0f
Haykal_fk <haykal.535240197@stu.untar.ac.id>
2025-11-24 12:55:20 +0700

    ubah

--- app/latihan/page.tsx
+++ app/latihan/page.tsx
@@ -1,14 +1,13 @@
 import prisma from '@/lib/prisma';
+export const dynamic = 'force-dynamic';
 
-// Ini komponen Server, bisa langsung ambil data dari DB
 export default async function LatihanPage() {
-  // Ambil semua data mahasiswa
   const mahasiswa = await prisma.mahasiswa.findMany();
 
   return (
     <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
       <h1>Data Mahasiswa dari SQLite</h1>
-      <p>Halaman ini hanya membaca data (Read Only) sesuai instruksi Latihan.</p>
+      <p>Halaman ini hanya membaca data (Read Only).</p>
 
       <ul style={{ marginTop: '20px' }}>
         {mahasiswa.map((mhs) => (
